f2515a5b2d97271daabc9a1ed1890df73958e607
Adriane Sutherland <asutherland91@gmail.com>
2023-07-10 16:33:28 -0700

    Add corrected imports to event details

--- src/Components/EventDetails/EventDetails.js
+++ src/Components/EventDetails/EventDetails.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import Header from '../ReusableComponents/Header/Header';
 import './EventDetails.css';
-import './Attendees/Attendees';
-import './Description/Description';
-import './EventInfo/EventInfo';
+import Attendees from './Attendees/Attendees';
+import Description from './Description/Description';
+import EventInfo from './EventInfo/EventInfo';
 import PropTypes from 'prop-types';
 
 export const EventDetails = ({ user, id, gameDetails, date, time, attendees, host, description }) => {
