a815d0d4269130ecca68ed3e8953de5f54832fa0
Adriane Sutherland <asutherland91@gmail.com>
2023-07-10 22:09:26 -0700

    Add some event info logic

--- src/Components/EventDetails/EventInfo/EventInfo.js
+++ src/Components/EventDetails/EventInfo/EventInfo.js
@@ -4,46 +4,42 @@ import Pills from '../../ReusableComponents/Pills/Pills';
 import Button from '../../ReusableComponents/Button/Button'
 import './EventInfo.css';
 
-export const EventInfo = ({ user, id, gameDetails, date, time, attendees, host}) => {
+export const EventInfo = ({ hostId, id, game, time, date, attendees, host}) => {
 
-  const renderRolePills = () => {
-    const isAttending = attendees.includes(user.id)
-    const isHost = host === user.id
-    const isFull = attendees.length === gameDetails.max_players
+
+  const isAttending = attendees.includes(attendees.id)
+  // const isHost = host === user.id
+
+  const renderRolePill = () => {
   
-    const renderPill = () => {
-      if (isHost) {
-        <Pills content='Host' />
-      }
-      else if (isAttending) {
+      // if (isHost) {
+      //   <Pills content='Host' />
+      // }
+      if (isAttending) {
         <Pills content='Attending' />
       } 
-      else if (isFull) {
-        <Pills content='Full' />
-      }
     }
 
   const renderButton = () => {
-    if(isHost) {
-      <Button content='Host Actions'/>
-    }
-    else if (!attendees.includes(user.id)) {
+    // if(isHost) {
+    //   <Button content='Host Actions'/>
+    // }
+    if (!attendees.includes(attendees.id)) {
       <Button content='Join'/>
     }
   }
 
   return (
     <div>
-        <h1 className='event-title'>{gameDetails.name}</h1>
+        <h1 className='event-title'>{game}</h1>
         <h2 className='event-date'>{date}</h2>
         <h2 className='event-time'>{time}</h2>
         <div className='event-pill-holder'>
-          { renderPill() }
+          { renderRolePill() }
         </div>
         <div className='event-button-holder'>
           { renderButton() }
         </div>
     </div>
   )
-  }
 }
