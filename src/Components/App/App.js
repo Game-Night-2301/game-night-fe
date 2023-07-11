2b8a6b94a0baa26217c4f2108679e7722bf6159b
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-07-11 16:11:02 -0400

    Add error message, update index queries to proper case

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -43,7 +43,7 @@ function App() {
   };
 
   if (loading) return <p>Loading...</p>; 
-  if (error) return <p>Error :</p>; 
+  if (error) return <p>Error : {error.message}</p>; 
 
   return (
     <Router>
