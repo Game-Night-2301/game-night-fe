b1a26571bedb64dcb8385237046f5b7b3c3efdd2
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-07-11 17:41:43 -0400

    Add small css changes

--- src/Components/ReusableComponents/Header/Header.js
+++ src/Components/ReusableComponents/Header/Header.js
@@ -11,16 +11,16 @@ const Header = ({logoutUser}) => {
 
   return (
     <header className="header">
-      <NavLink to="/browse" className="link">
+      <NavLink to="/browse" className="header-nav-link">
         <img src={logo} alt="Game Night Logo" className="logo" />
       </NavLink>
       <nav className="button-group">
-        <NavLink to="/Groups" className="nav-link">Groups</NavLink>
-        <NavLink to="/Collection" className="nav-link">Game Collection</NavLink>
-        <NavLink to="/Create" className="nav-link">Create</NavLink>
-        <NavLink to="/" className="nav-link" onClick={handleLogout}>Logout</NavLink>
+        <NavLink to="/Groups" className="header-nav-link">Groups</NavLink>
+        <NavLink to="/Collection" className="header-nav-link">Game Collection</NavLink>
+        <NavLink to="/Create" className="header-nav-link">Create</NavLink>
+        <NavLink to="/" className="header-nav-link" onClick={handleLogout}>Logout</NavLink>
       </nav>
-      <NavLink to="/Profile" className="nav-link">
+      <NavLink to="/Profile" className="header-nav-link">
         <img src={usericon} alt="User Icon" className='profile-link' />
       </NavLink>
     </header>
