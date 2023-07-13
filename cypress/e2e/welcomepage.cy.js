8d7f4268cb11235ccc70d5af4f309cff03cf5ca4
Ashlee Webb <ashlee.webb.18@gmail.com>
2023-07-13 14:33:35 -0400

    fix dropdown test

--- cypress/e2e/welcomepage.cy.js
+++ cypress/e2e/welcomepage.cy.js
@@ -22,7 +22,7 @@ beforeEach(() => {
       }
     }).as('getUserGames');
   });
-  cy.visit('https://game-night-fe.vercel.app/');
+  cy.visit('http://localhost:3000/');
 });
 
 
@@ -47,27 +47,19 @@ it('should display the browse page with the dropdown menu', () => {
   cy.wait('@getAllEvents').its('response.body').should('have.property', 'data');
   cy.get('.browse-event-container').should('be.visible');
   cy.get('.profile-link').click();
-  cy.get('.dropdown-menu').should('be.visible');
-  cy.get('.dropdown-menu').within(() => {
-    cy.contains('Create Event').should('be.visible');
-    cy.contains('Profile').should('be.visible');
-    cy.contains('Logout').should('be.visible');
-  });
+  cy.get('.menu-link').should('be.visible');
+  cy.get('.menu-link').contains('Logout').should('be.visible');
   cy.contains('Logout').click({ force: true });
   cy.get('.welcome-page-container').should('be.visible');
 });
 
 it('should display the browse page with the dropdown menu', () => {
-  cy.get('.welcome-button-container').find('button').contains('User 1').click();
+  cy.get('.welcome-button-container').find('button').contains('User 2').click();
   cy.get('.browse-event-container').should('be.visible');
   cy.get('.profile-link').click();
-  cy.get('.dropdown-menu').should('be.visible');
-  cy.get('.dropdown-menu').within(() => {
-    cy.contains('Create Event').should('be.visible');
-    cy.contains('Profile').should('be.visible');
-    cy.contains('Logout').should('be.visible');
-  });
+  cy.get('.menu-link').should('be.visible');
+  cy.get('.menu-link').should('be.visible');
+  cy.get('.menu-link').contains('Logout').should('be.visible');
   cy.contains('Logout').click({ force: true });
   cy.get('.welcome-page-container').should('be.visible');
 });
-
