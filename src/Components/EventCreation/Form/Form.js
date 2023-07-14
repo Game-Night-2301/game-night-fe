5da9c05176dd371f615caec642be12c86e3ac530
Adriane Sutherland <asutherland91@gmail.com>
2023-07-14 13:43:39 -0700

    Add updated form categories

--- src/Components/EventCreation/Form/Form.js
+++ src/Components/EventCreation/Form/Form.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import { useMutation } from '@apollo/client';
-import "./Form.css";
-import TextField from "@mui/material/TextField";
-import Button from "../../ReusableComponents/Button/Button";
-import MenuItem from "@mui/material/MenuItem";
-import dayjs from "dayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { TimePicker } from "@mui/x-date-pickers/TimePicker";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { createEventMutation } from "../../../queries";
+import './Form.css';
+import TextField from '@mui/material/TextField';
+import Button from '../../ReusableComponents/Button/Button';
+import MenuItem from '@mui/material/MenuItem';
+import dayjs from 'dayjs';
+import Header from '../../ReusableComponents/Header/Header';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { TimePicker } from '@mui/x-date-pickers/TimePicker';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { createEventMutation } from '../../../queries';
 
-const Form = ({logoutUser, loggedInUser}) => {
+const Form = ({ logoutUser, loggedInUser }) => {
   const [game, setGame] = useState('');
   const [category, setCategory] = useState('');
   const [address, setAddress] = useState('');
@@ -23,22 +24,37 @@ const Form = ({logoutUser, loggedInUser}) => {
   const [endTime, setEndTime] = useState(null);
   const [eventDescription, setEventDescription] = useState('');
 
-  const [createEvent, { data, loading, error }] = useMutation(createEventMutation);
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const onCreateEvent = async(input) => {
-    try {
-      const { data } = await createEvent({ variables: { input } });
-      console.log(data);
-    } catch (error) {
-      console.log(error);
-    }
+  const handleSuccess = (message) => {
+    setSuccessMessage(message);
+    setErrorMessage('');
   };
 
+  const handleError = (message) => {
+    setSuccessMessage('');
+    setErrorMessage(message);
+  };
+
+  const [createEvent, { data, loading, error }] =
+    useMutation(createEventMutation);
+
+  const onCreateEvent = async (input) => {
+    try {
+      const { data } = await createEvent({ variables: { input } });
+      handleSuccess('Your event is set, and your next adventure awaits!');
+    } catch (error) {
+      handleError(
+        'Critical fail! Your event was unable to be created, please try again!'
+      );
+    }
+  };
 
   const handleGameChange = (e) => {
     setGame(e.target.value);
   };
-  
+
   const handleDateChange = (date) => {
     setDate(date);
   };
@@ -77,7 +93,7 @@ const Form = ({logoutUser, loggedInUser}) => {
 
   const handleSubmit = () => {
     const newEvent = {
-      date: dayjs(date).format("YYYY/MM/DD"),
+      date: dayjs(date).format('YYYY/MM/DD'),
       address: address,
       city: city,
       state: state,
@@ -87,128 +103,151 @@ const Form = ({logoutUser, loggedInUser}) => {
       host: loggedInUser,
       game: 1,
       gameType: category,
-      startTime: dayjs(startTime).format("HH:mm:ss"),
-      endTime: dayjs(endTime).format("HH:mm:ss"),
-    }    
-    console.log(newEvent)
+      startTime: dayjs(startTime).format('HH:mm:ss'),
+      endTime: dayjs(endTime).format('HH:mm:ss'),
+    };
+    console.log(newEvent);
     onCreateEvent(newEvent);
   };
 
   return (
-    <form className="form">
-      <div className="container">
-        <h2 className="form-header">Game</h2>
-        <hr className="line" />
-        <div className="game-details">
-          <TextField
-            className="game-selector"
-            id="game"
-            value={game}
-            onChange={(e) => handleGameChange(e)}
-            select
-            label="Games"
-            sx={{margin: "1em"}}
-            helperText="Please select a game"
-          >
-            <MenuItem value={"Game 1"}>{"Game 1"}</MenuItem>
-            <MenuItem value={"Game 2"}>{"Game 2"}</MenuItem>
-            <MenuItem value={"Game 3"}>{"Game 3"}</MenuItem>
-          </TextField>
-          <TextField
-            id="category"
-            value={category}
-            select
-            onChange={(e) => handleCategoryChange(e)}
-            label="Category"
-            sx={{margin: "1em"}}
-            helperText="Please select a category"
-          >
-            <MenuItem value={"Category 1"}>{"Category 1"}</MenuItem>
-            <MenuItem value={"Category 2"}>{"Category 2"}</MenuItem>
-            <MenuItem value={"Category 3"}>{"Category 3"}</MenuItem>
-          </TextField>
-        </div>
-        <h2 className="form-header">Location</h2>
-        <hr className="line" />
-        <div className="location-details">
-          <TextField
-            required
-            onChange={(e) => handleAddressChange(e)}
-            id="address"
-            value={address}
-            label="Address"
-            sx={{margin: "1em"}}
-          />
-          <TextField
-            required
-            onChange={(e) => handleCityChange(e)}
-            id="city"
-            value={city}
-            label="City"
-            sx={{margin: "1em"}}
-          />
-          <TextField
-            required
-            onChange={(e) => handleStateChange(e)}
-            id="state"
-            value={state}
-            label="State"
-            sx={{margin: "1em"}}
-          />
-          <TextField
-            required
-            onChange={(e) => handleZipChange(e)}
-            id="zip"
-            value={zip}
-            label="Zip Code"
-            sx={{margin: "1em"}}
-          />
-          <h2 className="form-header">Date and Time</h2>
+    <div>
+      <Header logoutUser={logoutUser} />
+      <form className="form">
+        <div className="container">
+          <h2 className="form-header">Game</h2>
           <hr className="line" />
-          <div className="event-time-and-date">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker
-                label="Date"
-            required
-                value={date}
-                sx={{margin: "1em"}}
-                onChange={(newDate) => handleDateChange(newDate)}
-              />
-
-              <TimePicker
-                label="Start Time"
-            required
-                value={startTime}
-                sx={{margin: "1em"}}
-                onChange={(startTime) => handleStartTimeChange(startTime)}
-              />
-              <TimePicker
-                label="End Time"
-            required
-                value={endTime}
-                sx={{margin: "1em"}}
-                onChange={(endTime) => handleEndTimeChange(endTime)}
-              />
-            </LocalizationProvider>
+          <div className="game-details">
+            <TextField
+              className="game-selector"
+              id="game"
+              value={game}
+              onChange={(e) => handleGameChange(e)}
+              select
+              label="Games"
+              sx={{ margin: '1em' }}
+              helperText="Please select a game"
+            >
+              <MenuItem value={'Game 1'}>{'Game 1'}</MenuItem>
+              <MenuItem value={'Game 2'}>{'Game 2'}</MenuItem>
+              <MenuItem value={'Game 3'}>{'Game 3'}</MenuItem>
+            </TextField>
+            <TextField
+              id="category"
+              value={category}
+              select
+              onChange={(e) => handleCategoryChange(e)}
+              label="Category"
+              sx={{ margin: '1em' }}
+              helperText="Please select a category"
+            >
+              <MenuItem value={'Card Game'}>{'Card Game'}</MenuItem>
+              <MenuItem value={'Fantasy'}>{'Fantasy'}</MenuItem>
+              <MenuItem value={'Sci-Fi'}>{'Sci-Fi'}</MenuItem>
+              <MenuItem value={'City Builder'}>{'City Builder'}</MenuItem>
+              <MenuItem value={'Medieval'}>{'Medieval'}</MenuItem>
+              <MenuItem value={'Adventure'}>{'Adventure'}</MenuItem>
+              <MenuItem value={'Fighting'}>{'Fighting'}</MenuItem>
+              <MenuItem value={'Territory Building'}>
+                {'Territory Building'}
+              </MenuItem>
+              <MenuItem value={'Party Game'}>{'Party Game'}</MenuItem>
+              <MenuItem value={'Ancient'}>{'Ancient'}</MenuItem>
+              <MenuItem value={'Bluffing'}>{'Bluffing'}</MenuItem>
+              <MenuItem value={'Mythology'}>{'Mythology'}</MenuItem>
+              <MenuItem value={'Deduction'}>{'Deduction'}</MenuItem>
+              <MenuItem value={'Puzzle'}>{'Puzzle'}</MenuItem>
+              <MenuItem value={'Dice'}>{'Dice'}</MenuItem>
+              <MenuItem value={'Resource Management'}>
+                {'Resource Management'}
+              </MenuItem>
+              <MenuItem value={'Exploration'}>{'Exploration'}</MenuItem>
+              <MenuItem value={'Cooperative'}>{'Cooperative'}</MenuItem>
+            </TextField>
           </div>
+          <h2 className="form-header">Location</h2>
+          <hr className="line" />
+          <div className="location-details">
+            <TextField
+              required
+              onChange={(e) => handleAddressChange(e)}
+              id="address"
+              value={address}
+              label="Address"
+              sx={{ margin: '1em' }}
+            />
+            <TextField
+              required
+              onChange={(e) => handleCityChange(e)}
+              id="city"
+              value={city}
+              label="City"
+              sx={{ margin: '1em' }}
+            />
+            <TextField
+              required
+              onChange={(e) => handleStateChange(e)}
+              id="state"
+              value={state}
+              label="State"
+              sx={{ margin: '1em' }}
+            />
+            <TextField
+              required
+              onChange={(e) => handleZipChange(e)}
+              id="zip"
+              value={zip}
+              label="Zip Code"
+              sx={{ margin: '1em' }}
+            />
+            <h2 className="form-header">Date and Time</h2>
+            <hr className="line" />
+            <div className="event-time-and-date">
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  label="Date"
+                  required
+                  value={date}
+                  sx={{ margin: '1em' }}
+                  onChange={(newDate) => handleDateChange(newDate)}
+                />
+
+                <TimePicker
+                  label="Start Time"
+                  required
+                  value={startTime}
+                  sx={{ margin: '1em' }}
+                  onChange={(startTime) => handleStartTimeChange(startTime)}
+                />
+                <TimePicker
+                  label="End Time"
+                  required
+                  value={endTime}
+                  sx={{ margin: '1em' }}
+                  onChange={(endTime) => handleEndTimeChange(endTime)}
+                />
+              </LocalizationProvider>
+            </div>
+          </div>
+          <h2 className="form-header">Event Details</h2>
+          <hr className="line" />
+          <div className="event-details">
+            <TextField
+              id="outlined-multiline-static"
+              label="Details"
+              multiline
+              value={eventDescription}
+              rows={6}
+              sx={{ margin: '1em', width: '98%' }}
+              onChange={(e) => handleEventDescriptionChange(e)}
+            />
+          </div>
+          <Button className="button" text="Submit" onClick={handleSubmit} />
         </div>
-        <h2 className="form-header">Event Details</h2>
-        <hr className="line" />
-        <div className="event-details">
-          <TextField
-            id="outlined-multiline-static"
-            label="Details"
-            multiline
-            value={eventDescription}
-            rows={6}
-            sx={{margin: "1em",
-            width: "98%"}}
-            onChange={(e) => handleEventDescriptionChange(e)}
-          />
-        </div>
-        <Button className="button" text="Submit" onClick={handleSubmit} />
-      </div>
-    </form>
+        {successMessage && <p className="success-message">{successMessage}</p>}
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+      </form>
+    </div>
   );
 };
 
