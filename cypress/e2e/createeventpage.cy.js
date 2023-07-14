2ced7682dd7096b1d67f7e9dd1c88bed2e53da75
Trey Marshall <marshall.trey92@yahoo.com>
2023-07-14 12:27:57 -0500

    feat: Add cypress test for form checking contents.

--- cypress/e2e/createeventpage.cy.js
+++ cypress/e2e/createeventpage.cy.js
@@ -6,25 +6,27 @@ describe('Create Event Page', () => {
           req.reply({ data: getUser });
         }
       }).as('getUser');
-    });
 
-    cy.fixture('allEvents.json').then((getAllEvents) => {
-      cy.intercept('POST', 'https://game-night-backend-172o.onrender.com/graphql', (req) => {
-        if (req.body.operationName === 'getAllEvents') {
-          req.reply({ data: getAllEvents });
-        }
-      }).as('getAllEvents');
-    });
 
-    cy.fixture('getUserGames.json').then((getUserGames) => {
-      cy.intercept('POST', 'https://game-night-backend-172o.onrender.com/graphql', (req) => {
-        if (req.body.operationName === 'getUser') {
-          req.reply({ data: getUserGames });
-        }
-      }).as('getUserGames');
+      cy.fixture('allEvents.json').then((getAllEvents) => {
+        cy.intercept('POST', 'https://game-night-backend-172o.onrender.com/graphql', (req) => {
+          if (req.body.operationName === 'getAllEvents') {
+            req.reply({ data: getAllEvents });
+          }
+        }).as('getAllEvents');
+      });
+
+      cy.fixture('getUserGames.json').then((getUserGames) => {
+        cy.intercept('POST', 'https://game-night-backend-172o.onrender.com/graphql', (req) => {
+          if (req.body.operationName === 'getUser') {
+            req.reply({ data: getUserGames });
+          }
+        }).as('getUserGames');
+      });
+      cy.visit('https://game-night-fe.vercel.app/');
     });
-    cy.visit('https://game-night-fe.vercel.app/');
   });
+
 
   it('should display the create event page', () => {
     cy.get('.welcome-button-container').find('button').contains('User 1').click();
@@ -34,8 +36,28 @@ describe('Create Event Page', () => {
     cy.get('.menu-link').should('be.visible');
     cy.get('.menu-link').contains('Create Event').should('be.visible');
     cy.contains('Create Event').click({ force: true })
-    .then(() => {
-      cy.url().should('include', '/create');
+      .then(() => {
+        cy.url().should('include', '/create');
+      });
   });
-});
+
+
+  it("Checks contents of form", () => {
+    cy.get('.welcome-button-container').find('button').contains('User 1').click();
+    cy.get('.profile-link').click();
+    cy.get('.menu-link').should('be.visible');
+    cy.get('.menu-link').contains('Create Event').click();
+
+    cy.get("#game")
+    cy.get("#category")
+    cy.get("#address")
+    cy.get("#city")
+    cy.get("#state")
+    cy.get("#zip")
+    cy.get(".event-time-and-date > :nth-child(1) > .MuiInputBase-root")
+    cy.get(cy.get('.event-time-and-date > :nth-child(2) > .MuiInputBase-root'))
+    cy.get('.event-time-and-date > :nth-child(3) > .MuiInputBase-root')
+    cy.get("#outlined-multiline-static")
+    cy.get(".MuiButton-root").contains('Submit')
+  });
 });
