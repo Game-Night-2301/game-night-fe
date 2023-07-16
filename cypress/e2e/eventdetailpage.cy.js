c30bd9151a291d51da1d71296ae6fc4bc638705f
Trey Marshall <marshall.trey92@yahoo.com>
2023-07-15 19:11:19 -0500

    feat: Add element tests to eventdetailpage.

--- cypress/e2e/eventdetailpage.cy.js
+++ cypress/e2e/eventdetailpage.cy.js
@@ -46,9 +46,40 @@ beforeEach(() => {
   cy.get('.card').first().click();
 });
 
-it('should display the event page with the correct elements', () => {
-  cy.wait('@getEvent').then(() => {
-    cy.url().should('include', '/events/7');
+  it('should display the event page with the correct elements', () => {
+    cy.wait('@getEvent').then(() => {
+      cy.url().should('include', '/events/7');
+
+      cy.get('.event-title')
+        .contains('Wingspan')
+      cy.get('.pill')
+        .contains('Host')
+      cy.get('.event-date')
+        .contains('Saturday, October 21st')
+      cy.get('.event-time')
+        .contains('08:00 - 12:00')
+      cy.get('.event-button-holder > .MuiButtonBase-root')
+        .contains('Cancel Event') 
+      cy.get('.description-header')
+        .contains('Event Information')
+      cy.get('#panel1a-header > .MuiAccordionSummary-content > .MuiTypography-root')
+        .contains('Host Message')  
+      cy.get('#panel1a-content > .MuiAccordionDetails-root')
+        .contains('Ha ha, you fool! You fell victim to one of the classic blunders!')
+      cy.get('#panel2a-header > .MuiAccordionSummary-content > .MuiTypography-root')
+        .contains('Game Description').click()
+      cy.get('.scroll-wrapper > .MuiTypography-root')
+        .contains('Wingspan is a competitive, medium-weight, card-driven, engine-building board game from Stonemaier Games. You are bird enthusiasts—researchers, bird watchers, ornithologists, and collectors—seeking to discover and attract the best birds to your aviary.')
+      cy.get(':nth-child(5) > #panel2a-header > .MuiAccordionSummary-content > .MuiTypography-root')
+        .contains('Event Location').click()
+      cy.get('.leaflet-container')
+      cy.get('.attendee-header')
+        .contains('Attendees')
+      cy.get('[alt="11"]')
+        .trigger('mouseover')
+        .get('.MuiTooltip-tooltip')
+        .should('be.visible')
+        .contains('Herugar Bolger')                 
+    });
   });
-});
 });
